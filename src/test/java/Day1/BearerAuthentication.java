3468696426d90513d5675603295a388b2b3c4796
prajwaljeevangi66-sys <prajwaljeevangi66@gmail.com>
2026-01-18 14:07:27 +0530

    Update BearerAuthentication.java

--- src/test/java/Day1/BearerAuthentication.java
+++ src/test/java/Day1/BearerAuthentication.java
@@ -1,5 +1,28 @@
 package Day1;
 
+import static io.restassured.RestAssured.*;
+
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
 public class BearerAuthentication {
+	
+	@Test
+	
+	public void GetToken() {
+		given()
+		  .header("Authorization","Bearer ghp_MdvuDTgywxu86uHghgrip")
+		.when()
+		   .get("https://api.github.com/user/repos")
+		 .then()
+		    .statusCode(200)
+		    .body("name[0]",Matchers.equalTo("Sample-Repo")).log().body();
+		
+		
+		
+	}
+	
+	
+	
 
 }
