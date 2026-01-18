7b6c23198e3d489fd1e45a3275d979b456681987
prajwaljeevangi66-sys <prajwaljeevangi66@gmail.com>
2026-01-18 14:08:29 +0530

    Update ResponseBody.java

--- src/test/java/Day1/ResponseBody.java
+++ src/test/java/Day1/ResponseBody.java
@@ -1,5 +1,55 @@
 package Day1;
 
+import static io.restassured.RestAssured.*;
+
+import org.hamcrest.Matchers;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
 public class ResponseBody {
+	
+	@Test
+	public void  SResponseBody() {
+		
+		given()
+		 .when()
+		     .get("https://fakestoreapi.com/products/1")
+		  .then()
+		       .log().body();
+		
+	}
+//	"category": "men's clothing",
+//	@Test
+	
+//	public void stringResponseBody() {
+//		
+//		
+//		String responseBody = given()
+//				             .when()
+//				                 .get("https://fakestoreapi.com/products/1")
+//				                 .getBody().asString();
+//		Assert.assertTrue(responseBody.contains("men's clothing"));
+//		
+//	}
+	
+	
+	@Test
+	
+	public void Validate() {
+		
+		given()
+		     .when()
+		          .get("https://fakestoreapi.com/products/1")
+		      .then()
+		           
+		           .body("category",Matchers.equalTo("men  clothing")).log().body();
+		
+	}
+	
+	
+	
+	
 
 }
